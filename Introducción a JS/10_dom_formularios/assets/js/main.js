b39a28acfb8049a2ed804359e93aea3ff15c6f1d
Inés María O <inesmao@misena.edu.co>
2022-11-04 19:48:57 -0500

    eliminé archivos que no necesitabamos

--- Introducción a JS/10_dom_formularios/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const guardarPersona = () => {
-    const cajaNombres = document.getElementById('nombres').value;
-    const cajaApellidos = document.getElementById('apellidos').value;
-    const cajaCorreo = document.getElementById('correo').value;
-
-    const mostrarTexto = cajaNombres + '<br/>' + cajaApellidos + '<br/>' + cajaCorreo;
-    document.getElementById('valores').innerHTML = mostrarTexto;
-}
